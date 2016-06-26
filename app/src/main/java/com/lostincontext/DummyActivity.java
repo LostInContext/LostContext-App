dcec4628e94b979ee14d11c23511cdb5b17e2461
François Blavoet <francois.blavoet@gmail.com>
2016-06-26 18:24:36 +0200

    Initial commit

---
+++ app/src/main/java/com/lostincontext/DummyActivity.java
@@ -0,0 +1,14 @@
+package com.lostincontext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//Todo replace
+public class DummyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy);
+    }
+}
