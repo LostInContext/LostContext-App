dd91a669db527c48b67a98203a517e6aca955138
François Blavoet <francois.blavoet@gmail.com>
2016-07-15 17:05:19 +0200

    Tuned up the rule button

--- app/src/main/java/com/lostincontext/commons/widget/LostRadioButton.java
+++ app/src/main/java/com/lostincontext/commons/widget/LostRadioButton.java
@@ -1,32 +1,41 @@
 package com.lostincontext.commons.widget;
 
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.drawable.AnimatedVectorDrawable;
-import android.support.annotation.DrawableRes;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.support.annotation.ColorInt;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.lostincontext.R;
+import com.lostincontext.data.rules.FenceVM;
 import com.lostincontext.databinding.WidgetLostRadioButtonBinding;
-
-import static android.support.v4.content.ContextCompat.getDrawable;
 
 
 public class LostRadioButton extends RelativeLayout implements View.OnClickListener {
 
-    private AnimatedVectorDrawable tickToPlus;
-    private AnimatedVectorDrawable plusToTick;
 
-    private boolean tick;
+    public static final float SCALE_SELECTED = 1.2f;
+    private FenceVM fence;
+    private Callback callback;
+
+
+    public interface Callback {
+        void onLostRadioButtonClick(FenceVM fence, boolean selected);
+    }
+
+    private boolean selected;
 
     private final int elevationTarget;
+    @ColorInt private final int selectedColor;
+    @ColorInt private final int defaultColor;
 
-
-    @DrawableRes private int drawableRes;
     private WidgetLostRadioButtonBinding binding;
 
     public LostRadioButton(Context context) {
@@ -48,8 +57,6 @@ public class LostRadioButton extends RelativeLayout implements View.OnClickListe
         binding = WidgetLostRadioButtonBinding.inflate(factory, this, true);
         setOnClickListener(this);
 
-        tickToPlus = (AnimatedVectorDrawable) getDrawable(getContext(), R.drawable.avd_tick_to_plus);
-        plusToTick = (AnimatedVectorDrawable) getDrawable(getContext(), R.drawable.avd_plus_to_tick);
 
         elevationTarget = getResources().getDimensionPixelSize(R.dimen.lost_radio_button_elevation);
 
@@ -60,39 +67,75 @@ public class LostRadioButton extends RelativeLayout implements View.OnClickListe
                 0, 0);
 
         int res = a.getResourceId(R.styleable.LostRadioButton_src, -1);
-        if (res != -1) binding.imageView.setImageResource(res);
+        if (res != -1) binding.icon.setImageResource(res);
+        res = a.getResourceId(R.styleable.LostRadioButton_selected_color, Color.RED);
+        selectedColor = res;
 
         a.recycle();
 
+        defaultColor = ContextCompat.getColor(context, R.color.lost_radio_button_color);
 
     }
 
-    private void tick() {
-        if (tick) setPlus();
-        else setTick();
-        tick = !tick;
+    private void setState(boolean selected) {
+        if (selected == this.selected) return;
+        if (selected) {
+            binding.icon.setScaleX(SCALE_SELECTED);
+            binding.icon.setScaleY(SCALE_SELECTED);
+            binding.icon.setElevation(elevationTarget);
+
+        } else {
+            binding.icon.setScaleX(0);
+            binding.icon.setScaleY(0);
+            binding.icon.setElevation(0);
+        }
+    }
+
+    public void setContent(FenceVM fence,
+                           Callback callback,
+                           boolean selected) {
+        this.fence = fence;
+        this.callback = callback;
+        setState(selected);
     }
 
 
-    private void setTick() {
-        AnimatedVectorDrawable drawable = plusToTick;
-        binding.tick.setImageDrawable(drawable);
-        binding.imageView.animate().scaleX(1.2f).scaleY(1.2f).z(elevationTarget);
-        binding.tick.animate().z(elevationTarget);
-        drawable.start();
+    private void toggle() {
+        if (selected) setUnSelected();
+        else setSelected();
+        selected = !selected;
+        if (callback != null) callback.onLostRadioButtonClick(fence, selected);
+    }
+
+    private void setSelected() {
+        animate(SCALE_SELECTED, elevationTarget, defaultColor, selectedColor);
     }
 
 
-    private void setPlus() {
-        AnimatedVectorDrawable drawable = tickToPlus;
-        binding.tick.setImageDrawable(drawable);
-        binding.imageView.animate().scaleX(1f).scaleY(1f).z(0f);
-        binding.tick.animate().z(0f);
-        drawable.start();
+    private void setUnSelected() {
+        animate(1f, 0f, selectedColor, defaultColor);
+    }
+
+    private void animate(float targetScale, float elevationTarget, int fromColor, int toColor) {
+        binding.icon.animate()
+                .scaleX(targetScale)
+                .scaleY(targetScale)
+                .z(elevationTarget);
+        final GradientDrawable shapeDrawable = (GradientDrawable) binding.icon.getBackground();
+        ValueAnimator textAnimator = ValueAnimator.ofArgb(fromColor,
+                                                          toColor);
+        textAnimator.setDuration(500);
+        textAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int animatedValue = (Integer) valueAnimator.getAnimatedValue();
+                shapeDrawable.setColor(animatedValue);
+            }
+        });
+        textAnimator.start();
     }
 
 
     @Override public void onClick(View view) {
-        tick();
+        toggle();
     }
 }
