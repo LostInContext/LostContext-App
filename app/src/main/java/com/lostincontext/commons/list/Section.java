dbe949cf7a903667ceddd2498692708e0ee3f539
François Blavoet <francois.blavoet@gmail.com>
2016-07-16 23:54:41 +0200

    Adds premices of activity list

--- app/src/main/java/com/lostincontext/commons/list/Section.java
+++ app/src/main/java/com/lostincontext/commons/list/Section.java
@@ -5,12 +5,18 @@ import java.util.List;
 
 public class Section<Model> {
 
+
+    public Section(String title, List<Model> models) {
+        this.title = title;
+        this.models = models;
+    }
+
     private String title;
     private List<Model> models;
 
 
     public int size() {
-        return models.size();
+        return models.size() + 1;
     }
 
     public Model get(int i) {
--- app/src/main/java/com/lostincontext/rulescreation/RulesCreationPresenter.java
+++ app/src/main/java/com/lostincontext/rulescreation/RulesCreationPresenter.java
@@ -1,10 +1,17 @@
 package com.lostincontext.rulescreation;
 
 import android.content.Intent;
+import android.graphics.Color;
 
+import com.lostincontext.R;
+import com.lostincontext.commons.list.Section;
 import com.lostincontext.data.FenceCreator;
 import com.lostincontext.data.location.repo.LocationRepository;
 import com.lostincontext.rulescreation.display.EditLocationDialog;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -19,6 +26,16 @@ public class RulesCreationPresenter implements RulesCreationContract.Presenter {
     @Override public void start() {
 
        // view.showDialog();
+
+        List<FenceCreator> activities = new ArrayList<>();
+        activities.add(new FenceCreator("running", R.drawable.ic_home_24, Color.RED) );
+        activities.add(new FenceCreator("running", R.drawable.ic_home_24, Color.RED) );
+        activities.add(new FenceCreator("running", R.drawable.ic_home_24, Color.RED) );
+
+        Section<FenceCreator> fenceCreatorSection = new Section<>("Activity", activities );
+
+
+        view.setSections(Arrays.asList(fenceCreatorSection));
     }
 
     @Override public void onActivityResult(int requestCode, int resultCode, Intent data) {
