028617fa58a4be9c0cfc8f040340a8620900698d
François Blavoet <francois.blavoet@gmail.com>
2016-08-03 08:33:54 +0200

    Polish the notification a little bit

--- app/src/main/java/com/lostincontext/utils/Notifications.java
+++ app/src/main/java/com/lostincontext/utils/Notifications.java
@@ -6,11 +6,13 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 
 import com.lostincontext.R;
 import com.lostincontext.data.playlist.Playlist;
 import com.lostincontext.that.PlayReceiver;
 
+import static android.app.Notification.DEFAULT_ALL;
 import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class Notifications {
@@ -28,7 +30,11 @@ public class Notifications {
                 .setContentText("Launch playlist?")
                 .setSmallIcon(R.drawable.ic_music_note_24)
                 .setContentIntent(pIntent)
+                .setColor(ContextCompat.getColor(context, R.color.colorAccent))
+                .setDefaults(DEFAULT_ALL)
+                .setPriority(Notification.PRIORITY_HIGH)
                 .setAutoCancel(true)
+                .setCategory(NotificationCompat.CATEGORY_REMINDER)
                 .addAction(getPlayAction(context, playlist))
                 .build();
 
@@ -46,8 +52,8 @@ public class Notifications {
         int KEY_PLAY = 9877;
         PendingIntent pendingIntentPlay = PendingIntent.getBroadcast(context, KEY_PLAY, playReceive, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        return new NotificationCompat.Action.Builder(R.drawable.ic_tick_16,
-                                                     ACTION_PLAY,
+        return new NotificationCompat.Action.Builder(R.drawable.ic_play_24,
+                                                     context.getString(R.string.play),
                                                      pendingIntentPlay).build();
     }
 
