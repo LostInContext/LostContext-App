62063ffae3ff7d48b0e44854bb64fc09d4a8dbe3
François Blavoet <francois.blavoet@gmail.com>
2016-08-23 16:49:34 +0200

    Better DataBinding integration & build perfs
    The databinding compiler is in fact tied to the build tools version -_-
    It makes sense but it is not documented anywhere.

--- app/src/main/java/com/lostincontext/utils/Bindings.kt
+++ app/src/main/java/com/lostincontext/utils/Bindings.kt
@@ -1,18 +1,11 @@
 package com.lostincontext.utils
 
 
-import android.content.Context
 import android.databinding.BindingAdapter
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
 import android.support.annotation.DrawableRes
 import android.widget.ImageView
-
 import com.bumptech.glide.Glide
-import com.lostincontext.R
 import com.lostincontext.data.playlist.Playlist
-import com.lostincontext.ruledetails.items.FenceItem
 
 
 @BindingAdapter("resource")
@@ -24,9 +17,4 @@ fun setSrc(imageView: ImageView, @DrawableRes res: Int) {
 @BindingAdapter("glide_playlist")
 fun setImageUrl(imageView: ImageView, playlist: Playlist) {
     Glide.with(imageView.context).load(playlist).into(imageView)
-}
-
-fun getWhenItemBackground(context: Context, item: FenceItem): Drawable {
-    if (item.isWhen()) return ColorDrawable(Color.TRANSPARENT)
-    return context.getDrawable(R.drawable.app_btn_background_material)
 }
