488582805b75302029a05e3db60f7fb2cb6eb9ea
François Blavoet <fb@deezer.com>
2016-09-07 10:35:48 +0200

    Atomic needs a way to wrap the not case

--- app/src/main/java/com/lostincontext/data/rulesV2/AtomicCondition.kt
+++ app/src/main/java/com/lostincontext/data/rulesV2/AtomicCondition.kt
@@ -1,9 +1,23 @@
 package com.lostincontext.data.rulesV2
 
 import com.lostincontext.data.rules.FenceVM
+import com.lostincontext.data.rules.NotFenceVM
 
 
-data class AtomicCondition(val fence : FenceVM)
+data class AtomicCondition(private val fence: FenceVM, private val modifier: Modifier) {
 
-// atomic condition == fenceVM ???
-// not -> just another FenceVM after all
+    enum class Modifier { NONE, NOT }
+
+    fun getAtomic(): FenceVM {
+        when (modifier) {
+
+            Modifier.NONE -> return fence
+            Modifier.NOT -> return NotFenceVM(fence)
+        }
+
+    }
+
+
+}
+
+
