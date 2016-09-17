bac9b69040d0921bfaafe93aa13d679b559cf864
François Blavoet <francois.blavoet@gmail.com>
2016-09-17 14:31:28 +0200

    Don't expose Retrofit internals to callers

--- app/src/main/java/com/lostincontext/utils/Retrofit.kt
+++ app/src/main/java/com/lostincontext/utils/Retrofit.kt
@@ -5,15 +5,24 @@ import retrofit2.Callback
 import retrofit2.Response
 
 
-inline fun <T> Call<T>.enqueue(crossinline success: (response: Response<T>) -> Unit,
-                               crossinline failure: (t: Throwable) -> Unit) {
+const val FAILURE_CODE = -1
+
+inline fun <T> Call<T>.enqueue(crossinline success: (response: T) -> Unit,
+                               crossinline failure: (error: Int) -> Unit) {
     enqueue(object : Callback<T> {
         override fun onResponse(call: Call<T>, response: Response<T>) {
-            success(response)
+
+            if (response.isSuccessful) {
+                success(response.body())
+            } else {
+                logE("Retrofit") { "unsuccessful call : code ${response.code()}" }
+                failure(response.code())
+            }
         }
 
         override fun onFailure(call: Call<T>, t: Throwable) {
-            failure(t)
+            logE("Retrofit") { t.message ?: "empty error" }
+            failure(FAILURE_CODE)
         }
 
     })
