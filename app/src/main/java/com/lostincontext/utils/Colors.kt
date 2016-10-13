3ae53e837d7006d4d5f13c742178a9b4e0d46b81
François Blavoet <francois.blavoet@gmail.com>
2016-10-13 23:52:13 +0200

    Gamma Evaluator
    ArgEvaluator is not that great for color animations. It creates very
    ugly color jumps that just look wrong.
    
    This is because RGB colors are gamma encoded, their variation follow a
    power law, so a basic linear interpolation does not work. It leads to
    ugly color jumps.
    
    We can solve this by converting the color components to linear encoded
    values, interpolate and then bring them back.

--- app/src/main/java/com/lostincontext/utils/Colors.kt
+++ app/src/main/java/com/lostincontext/utils/Colors.kt
@@ -3,7 +3,6 @@ package com.lostincontext.utils
 
 import android.animation.ValueAnimator
 import android.content.res.ColorStateList
-import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
 import android.support.annotation.ColorInt
@@ -11,6 +10,7 @@ import android.view.View
 import android.view.animation.Interpolator
 import android.widget.ImageView
 import android.widget.TextView
+import com.lostincontext.commons.animation.GammaEvaluator
 
 
 fun View.animateBackgroundColor(@ColorInt from: Int,
@@ -62,21 +62,11 @@ private inline fun animate(@ColorInt from: Int,
                            duration: Long,
                            interpolator: Interpolator?,
                            crossinline callback: (valueAnimator: ValueAnimator) -> Unit) {
-    val valueAnimator = ValueAnimator.ofArgb(from, to)
+    val valueAnimator = ValueAnimator()
+    valueAnimator.setIntValues(from, to)
+    valueAnimator.setEvaluator(GammaEvaluator.getInstance())
     valueAnimator.addUpdateListener { valueAnimator -> callback.invoke(valueAnimator) }
     if (interpolator != null) valueAnimator.interpolator = interpolator
     valueAnimator.setDuration(duration)
             .start()
-}
-
-private fun blendColors(@ColorInt from: Int,
-                        @ColorInt to: Int,
-                        ratio: Float): Int {
-    val inverseRatio = 1f - ratio
-
-    val r = (Color.red(to) * ratio + Color.red(from) * inverseRatio).toInt()
-    val g = (Color.green(to) * ratio + Color.green(from) * inverseRatio).toInt()
-    val b = (Color.blue(to) * ratio + Color.blue(from) * inverseRatio).toInt()
-
-    return Color.rgb(r, g, b)
 }
