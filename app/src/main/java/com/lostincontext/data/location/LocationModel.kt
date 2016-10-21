9a373363024c2953c6095936cac40f6f7e895c7f
strabelsi <strabelsi@deezer.com>
2016-10-21 14:53:14 +0200

    Fixed loading location
    Fixed adding location (must be tested with an unknown location)

--- app/src/main/java/com/lostincontext/data/location/LocationModel.kt
+++ app/src/main/java/com/lostincontext/data/location/LocationModel.kt
@@ -1,13 +1,15 @@
 package com.lostincontext.data.location
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.google.android.gms.maps.model.LatLng
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class LocationModel(var placeName: String,
                          var latitude: Double,
                          var longitude: Double) {
-
+    @JsonIgnore
     fun getLatLng(): LatLng = LatLng(latitude, longitude)
 }
 
