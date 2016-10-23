5465388b0507f9edc11d80c9da1d2b85713085de
François Blavoet <francois.blavoet@gmail.com>
2016-10-24 01:57:44 +0200

    Add Gradient Transformation for playlist item
    Still need to setup the gradient values in order to make it a material
    gradient protection for images

--- app/src/main/java/com/lostincontext/utils/Bindings.kt
+++ app/src/main/java/com/lostincontext/utils/Bindings.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable
 import android.support.annotation.DrawableRes
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
 import com.lostincontext.commons.images.DeezerImage
 
 
@@ -28,5 +29,17 @@ fun setImageUrl(imageView: ImageView, image: DeezerImage?, placeholder: Drawable
     Glide.with(imageView.context)
             .load(image)
             .placeholder(placeholder)
+            .into(imageView)
+}
+
+
+@BindingAdapter("deezer_image", "placeholder", "transformation")
+fun setImageUrl(imageView: ImageView, image: DeezerImage?,
+                placeholder: Drawable,
+                transformation: BitmapTransformation) {
+    Glide.with(imageView.context)
+            .load(image)
+            .placeholder(placeholder)
+            .transform(transformation)
             .into(imageView)
 }
