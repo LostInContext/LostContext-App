9d1b5a318da8ff037e6b8d4184dda7fbfaea8629
François Blavoet <francois.blavoet@gmail.com>
2016-10-28 01:19:04 +0200

    Add AtomicConditionItem

---
+++ app/src/main/java/com/lostincontext/rulecreate/AtomicConditionItem.kt
@@ -0,0 +1,46 @@
+package com.lostincontext.rulecreate
+
+import com.genius.groupie.Item
+import com.lostincontext.R
+import com.lostincontext.data.rules.FenceNamer
+import com.lostincontext.data.rulesV2.AtomicCondition
+import com.lostincontext.databinding.ItemAtomicConditionBinding
+
+
+class AtomicConditionItem constructor(val callback: Callback,
+                                      val atomic: AtomicCondition,
+                                      val isFirst: Boolean,
+                                      val namer: FenceNamer) :
+        Item<ItemAtomicConditionBinding>() {
+
+    interface Callback {
+        fun onDeleteButtonClick(atomic: AtomicCondition)
+        fun onToggleClick(atomic: AtomicCondition)
+    }
+
+
+    override fun bind(viewBinding: ItemAtomicConditionBinding, position: Int) {
+        viewBinding.callback = callback
+        viewBinding.viewModel = this
+    }
+
+    override fun getLayout() = R.layout.item_condition
+
+
+    fun getModifierRes(): Int {
+        if (isFirst) {
+            when (atomic.modifier) {
+                AtomicCondition.Modifier.NONE -> return R.string.`when`
+                AtomicCondition.Modifier.NOT -> return R.string.when_not
+            }
+        } else {
+            when (atomic.modifier) {
+                AtomicCondition.Modifier.NONE -> return R.string.and
+                AtomicCondition.Modifier.NOT -> return R.string.and_not
+            }
+        }
+    }
+
+    fun getName() = atomic.fence.name(namer)
+
+}
