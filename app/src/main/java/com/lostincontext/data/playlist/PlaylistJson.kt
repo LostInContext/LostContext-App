2e1ab4aa8ee369c36166e410565a77b0863f25bc
François Blavoet <francois.blavoet@gmail.com>
2016-10-30 22:50:35 +0100

    Try-out PaperParcel
    We need less boilerplate if we are going to send conditions through
    parcels.

--- app/src/main/java/com/lostincontext/data/playlist/PlaylistJson.kt
+++ app/src/main/java/com/lostincontext/data/playlist/PlaylistJson.kt
@@ -4,4 +4,4 @@ package com.lostincontext.data.playlist
 data class PlaylistJson(val id: Long,
                         val title: String,
                         val picture_small: String,
-                        val creator: Creator)
+                        val creator: Author)
