5e360a4141ed4ad51f345e65731a1e4ea40e0e1c
François Blavoet <francois.blavoet@gmail.com>
2016-11-01 20:54:38 +0100

    Allow to delete a rule with longClick
    Temporary feature, we will implement the bin later on

--- app/src/main/java/com/lostincontext/mainscreen/MainScreenAdapter.kt
+++ app/src/main/java/com/lostincontext/mainscreen/MainScreenAdapter.kt
@@ -1,24 +1,30 @@
 package com.lostincontext.mainscreen
 
 import android.support.annotation.IdRes
+import android.support.v7.util.DiffUtil
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.lostincontext.R
 import com.lostincontext.commons.list.EmptyListCallback
 import com.lostincontext.commons.list.StatefulAdapter
+import com.lostincontext.commons.list.StatefulAdapter.ContentState.CONTENT
 import com.lostincontext.commons.list.StatefulAdapter.ContentState.LOADING
 import com.lostincontext.commons.list.ViewHolder
 import com.lostincontext.data.rulesV2.Rule
 
 
-class MainScreenAdapter(private val emptyListCallback: EmptyListCallback) : StatefulAdapter(LOADING) {
+class MainScreenAdapter(private val emptyListCallback: EmptyListCallback,
+                        private val callback: MainScreenViewHolder.Callback)
+    : StatefulAdapter(LOADING) {
 
     private var rules: List<Rule> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, @IdRes viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         when (viewType) {
-            R.id.view_type_standard -> return MainScreenViewHolder.create(layoutInflater, parent)
+            R.id.view_type_standard -> return MainScreenViewHolder.create(layoutInflater,
+                                                                          parent,
+                                                                          callback)
 
             R.id.view_type_loading -> return buildLoadingViewHolder(layoutInflater,
                                                                     parent)
@@ -60,5 +66,34 @@ class MainScreenAdapter(private val emptyListCallback: EmptyListCallback) : Stat
         else
             setCurrentState(StatefulAdapter.ContentState.CONTENT, true)
     }
+
+    /*
+        fun setRules(newRules: List<Rule>) {
+
+        if (newRules.isEmpty()) {
+            rules = newRules
+            setCurrentState(StatefulAdapter.ContentState.EMPTY)
+            return
+        }
+
+        currentState = CONTENT
+
+        val diff = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun areItemsTheSame(oldItemPosition: Int,
+                                         newItemPosition: Int) = rules[oldItemPosition].key == newRules[newItemPosition].key
+
+
+            override fun getOldListSize() = rules.size
+
+            override fun getNewListSize() = newRules.size
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = true
+
+        })
+
+        this.rules = newRules
+        diff.dispatchUpdatesTo(this)
+    }
+     */
 
 }
